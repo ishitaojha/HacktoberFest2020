a944fb018210587ffa3f4e12ebb535f4f422f1c5
nibbithecoder <59393136+nibbithecoder@users.noreply.github.com>
2020-10-24 12:40:23 +0530

    layout changed

--- Java/factorial_java.java
+++ Java/factorial_java.java
@@ -1,11 +1,12 @@
-class FactorialExample2{  
- static int factorial(int n){    
-  if (n == 0)    
+import java.util;
+ public class FactorialExample2{  
+  public static void main (String args [])
+  int factorial (int n) ;{    //initialize an integer for input number
+  if (n == 0)   //check if it is equal to zero 
     return 1;    
   else    
     return(n * factorial(n-1));    
- }    
- public static void main(String args[]){  
+   {  
   int i,fact=1;  
   int number=4;//It is the number to calculate factorial    
   fact = factorial(number);   
