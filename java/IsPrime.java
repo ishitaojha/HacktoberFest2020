590c250c538fa8419703ba95210fc114806bd1fb
nibbithecoder <59393136+nibbithecoder@users.noreply.github.com>
2020-10-24 12:49:13 +0530

    explained the functions and commands
    any integer if divisible by 2 gives output 0 , can't be prime as it is even .
    The exception is 2 itself .

--- java/IsPrime.java
+++ java/IsPrime.java
@@ -1,12 +1,12 @@
 public class isPrime(){
 public static boolean isPrime(int number){
   int cont = 2;
-  boolean prime=true;
+  boolean prime=true; // if the above condition is satisfied then boolean value will become true
   while ((prime) && (cont!=number)){
-    if (number % cont == 0)
-      prime = false;
-    cont++;
+    if (number % cont == 0) // if number is divisible by 2 then it's not prime , because it's even .
+      prime = false; // value will become false 
+    cont++; // counte incremented 
   }
-  return prime;
+  return prime; 
 }
 }
